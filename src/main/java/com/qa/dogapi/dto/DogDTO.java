47a46ff5082cca4d88ec3062c0bdc172e0408efc
chrisYiangou <chrisyiangou100@gmail.com>
2022-09-02 15:56:07 +0100

    Finished DogDTO

--- src/main/java/com/qa/dogapi/dto/DogDTO.java
+++ src/main/java/com/qa/dogapi/dto/DogDTO.java
@@ -2,4 +2,23 @@ package com.qa.dogapi.dto;
 
 public class DogDTO {
 
+	private Integer dogId;
+	private String name;
+	private String trainerName;
+	
+	public DogDTO(Integer dogId, String name, String trainerName) {
+		super();
+		this.dogId = dogId;
+		this.name = name;
+		this.trainerName = trainerName;
+	}
+	
+	public DogDTO(String name, String trainerName) {
+		
+		this.dogId = dogId;
+		this.name = name;
+		this.trainerName = trainerName;
+	}
+
+	
 }
